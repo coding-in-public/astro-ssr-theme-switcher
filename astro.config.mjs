5e587dd18bb3aa2e4d50037c964723dcc7d09ec7
Chris Pennington <chris@codinginpublic.dev>
2023-11-16 10:10:51 -0700

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  output: "hybrid",
+  adapter: netlify(),
+});
